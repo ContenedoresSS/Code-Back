562aa5c2cb57d223eedba085c8007ca73ce2194a
Charly3012 <carlocoronadosilva975@gmail.com>
2026-06-05 07:15:43 -0600

    chore: update seed to init new resources (activities, subjects, test cases, workspaces)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -35,12 +35,13 @@ async function main() {
     });
   }
 
-  console.log("Creating default user...");
+  console.log("Creating users...");
 
   const saltRounds = 10;
   const defaultPassword = "AdminPassword123!!";
   const hashedPassword = await bcrypt.hash(defaultPassword, saltRounds);
 
+  // Usuario God
   const adminUser = await prisma.user.upsert({
     where: { email: "admin@admin.com" },
     update: {},
@@ -54,7 +55,23 @@ async function main() {
       },
     },
   });
-  console.log(`God user created: ${adminUser.email}`);
+  console.log(`God user created: ${adminUser.email} with password: ${defaultPassword}`);
+
+  //Usuario Teacher
+  const teacherUser = await prisma.user.upsert({
+    where: { email: "profesor@uady.mx" },
+    update: {},
+    create: {
+      email: "profesor@uady.mx",
+      name: "Alan",
+      lastName: "Turing",
+      passwordHash: hashedPassword,
+      role: {
+        connect: { name: "Teacher" },
+      },
+    },
+  });
+  console.log(`Teacher user created: ${teacherUser.email} with password: ${defaultPassword}`);
 
   console.log("Adding default languages (C++, Python, Node.js)...");
   const languages = [
@@ -89,7 +106,74 @@ async function main() {
     });
   }
 
-  console.log(`Successfully seed`);
+  const cppLang = await prisma.programmingLanguage.findUnique({
+    where: { name_version: { name: "C++", version: "13.2" } },
+  });
+
+  if (cppLang) {
+    console.log("Creating default Subject...");
+    let subject = await prisma.subject.findFirst({
+      where: { name: "Estructuras de Datos", userId: teacherUser.id },
+    });
+
+    if (!subject) {
+      subject = await prisma.subject.create({
+        data: {
+          name: "Estructuras de Datos",
+          userId: teacherUser.id,
+        },
+      });
+    }
+
+    console.log("Creating default Activity and Test Cases...");
+    let activity = await prisma.activity.findFirst({
+      where: { title: "Suma de dos números", subjectId: subject.id },
+    });
+
+    if (!activity) {
+      activity = await prisma.activity.create({
+        data: {
+          professorId: teacherUser.id,
+          subjectId: subject.id,
+          languageId: cppLang.id,
+          title: "Suma de dos números",
+          description:
+            "Escribe un programa en C++ que lea dos enteros por entrada estándar y devuelva su suma.",
+          maxAttempts: 5,
+          allowCopy: true,
+          allowPaste: true,
+          starterCode: [
+            {
+              name: "main.cpp",
+              // "int main() { \n // Tu codigo aqui \n return 0; \n}"
+              content:
+                "I2luY2x1ZGUgPGlvc3RyZWFtPgp1c2luZyBuYW1lc3BhY2Ugc3RkOwppbnQgbWFpbigpIHsKICAgIC8vIFR1IGNvZGlnbyBhcXVpCiAgICByZXR1cm4gMDsKfQ==",
+            },
+          ],
+        },
+      });
+
+      await prisma.testCase.createMany({
+        data: [
+          {
+            activityId: activity.id,
+            input: "NQo3", // "5\n7"
+            expectedOutput: "MTI=", // "12"
+            isHidden: false,
+          },
+          {
+            activityId: activity.id,
+            input: "MTAwCi0yNQ==", // "100\n-25"
+            expectedOutput: "NzU=", // "75"
+            isHidden: true,
+          },
+        ],
+      });
+      console.log(`Activity created with ID: ${activity.id}`);
+    }
+  }
+
+  console.log(`Successfully seeded database 🌱`);
 }
 
 main()
